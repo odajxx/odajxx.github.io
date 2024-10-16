29aa18b582ca840c9090da688282b45eab605759
odaj <sendtojamal@gmail.com>
2024-10-16 17:56:17 +0100

    extended the javascript function for theme

--- script.js
+++ script.js
@@ -19,15 +19,18 @@ document.querySelectorAll('.nav-links').forEach(link => {
 });
 
 
-// icon onclick code
+var icon = document.getElementById("icon");
 
-var icon = document.getElementById("icon")
-
-icon.onclick = function(){
+icon.onclick = function() {
     document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")){
-        icon.src = "/images/sun.png";
-    }else{
-        icon.src = "/images/moon.png";
+
+    if (document.body.classList.contains("dark-theme")) {
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+    } else {
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
     }
 }
+
+
